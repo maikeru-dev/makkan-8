4a04dda355ba87b0c7a3015e0c42bf7e931d50bf
randomcmd <adrian@randomcmd.dev>
2023-06-06 14:22:25 +0200

    Fixed settings and timer testing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 import java.awt.*;
 import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
         Display display = new Display();
-        display.changeColour(Color.BLUE);
         RAM ram = new RAM(Settings.RAM_SIZE);
         ram.loadROMFile(0x200, new File("resources/IBM Logo.ch8"));
+
+        Timer timer = new Timer();
+        AtomicInteger secondTimer = new AtomicInteger(360); // count this down at 60Hz
+
+        var task = new TimerTask() {
+            @Override
+            public void run() {
+                secondTimer.decrementAndGet();
+                System.out.println("Second timer: " + secondTimer);
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 2000, 2000);
     }
 }
--- src/Settings.java
+++ src/Settings.java
@@ -1,5 +1,6 @@
 public class Settings {
     public static final String PROGRAM_TITLE = "makkan-8";
+    public static final int DISPLAY_MULTIPLIER = 16;
 
     // CHIP 8 SPECS
     public static final boolean SUPER_CHIP_PRESENT = false;
