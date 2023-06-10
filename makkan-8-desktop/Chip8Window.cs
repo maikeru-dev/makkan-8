dab9ea01da72f4344812ffe506c55f6f5a1ccfc2
randomcmd <adrian@randomcmd.dev>
2023-06-10 16:26:29 +0200

    Added desktop monogame renderer

---
+++ makkan-8-desktop/Chip8Window.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace makkan_8_desktop;
+
+public class Chip8Window : Game
+{
+    private GraphicsDeviceManager Graphics;
+    private Texture2D Pixel;
+    
+    public Chip8Window()
+    {
+        Graphics = new GraphicsDeviceManager(this);
+    }
+
+    protected override void Initialize()
+    {
+        Pixel = new Texture2D(GraphicsDevice, 64, 64);
+        
+        // fill texture with random pixels
+        Color[] colors = new Color[64 * 64];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = new Color(
+                (byte)Random.Shared.Next(255), // G
+                (byte)Random.Shared.Next(255), // R
+                (byte)Random.Shared.Next(255), // B
+                (byte)Random.Shared.Next(255)); // A
+        }
+        Pixel.SetData(colors);
+        base.Initialize();
+    }
+
+    protected override void Update(GameTime deltaTime)
+    {
+        base.Update(deltaTime);
+    }
+
+    protected override void Draw(GameTime deltaTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        SpriteBatch spriteBatch = new SpriteBatch(GraphicsDevice);
+        spriteBatch.Begin(samplerState : SamplerState.PointClamp);
+        var windowWidth = GraphicsDevice.Viewport.Width;
+        var windowHeight = GraphicsDevice.Viewport.Height;
+        spriteBatch.Draw(Pixel, new Rectangle(0, 0, windowWidth, windowHeight), Color.White);
+        spriteBatch.End();
+        
+        base.Draw(deltaTime);
+    }
+}
---
+++ makkan-8-desktop/Program.cs
@@ -0,0 +1,4 @@
+﻿using makkan_8_desktop;
+
+using var chip8Window = new Chip8Window();
+chip8Window.Run();
