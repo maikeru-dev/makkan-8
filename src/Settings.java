4eca25c9f9e685890d0833439f7e3a9c12613ba7
Maikeru <63614642+maikeru-dev@users.noreply.github.com>
2023-06-10 14:19:20 +0100

    Removing old java folder from cs branch

--- src/Settings.java
+++
@@ -1,9 +0,0 @@
-public class Settings {
-    public static final String PROGRAM_TITLE = "makkan-8";
-
-    public static final int DISPLAY_MULTIPLIER = 8;
-
-    // CHIP 8 SPECS
-    public static final boolean SUPER_CHIP_PRESENT = true;
-    public static final int RAM_SIZE = 4096;
-}
